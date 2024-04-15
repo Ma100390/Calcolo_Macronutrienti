fe6d940ed79e71488711a1782fffe897e3626760
Manuel <magini15@gmail.com>
2024-04-15 18:32:00 +0200

    Aggiornament

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,6 +30,7 @@ export class AppComponent {
   resultMessage6: string = '';
 
   obiettivoCalorie: number | undefined;
+  calcoloP: number | undefined;
 
   calculateBMI(): void {
    if (this.age && this.peso && this.gender && this.altezza && this.giorni && this.obiettivo) {
@@ -52,22 +53,54 @@ export class AppComponent {
         const totalCalories = bmr * this.giorni;
         if (this.obiettivo === 'perdere') {
           // Perdere Peso (Lose Weight) option selected
-          this.obiettivoCalorie = totalCalories - 500;
-          this.grassi = (this.obiettivoCalorie * 0.25) / 9;
-          this.proteine = ( this.obiettivoCalorie) * 0.30 / 4;
-          this.carboidrati = ( this.obiettivoCalorie) * 0.45 / 4;
+          this.obiettivoCalorie = totalCalories - 500; // Calorie obiettivo per perdere peso
+        
+          // Calcolo delle proteine in grammi
+          // 2.1 grammi di proteine per chilogrammo di peso corporeo
+          this.proteine = 2.1 * this.peso; // Peso corporeo in kg
+         
+          // Calcolo delle calorie provenienti dalle proteine
+          const proteineS = this.proteine * 4; // 1g proteine = 4 calorie
+          
+          // Calcolo dei grassi in grammi
+          // 30% delle calorie totali provengono dai grassi
+          const calorieGrassi = 0.30 * this.obiettivoCalorie;
+          this.grassi = calorieGrassi / 9; // 1g grassi = 9 calorie
+        
+          // Calcolo dei carboidrati in grammi
+          // Il resto delle calorie provengono dai carboidrati
+          const calorieCarboidrati = proteineS + calorieGrassi;
+          const totaleCalarie = this.obiettivoCalorie - calorieCarboidrati 
+          this.carboidrati = totaleCalarie / 4; // 1g carboidrati = 4 calorie
+        
         } else if (this.obiettivo === 'nulla') {
           // Mantenere Peso (Maintain Weight) option selected
           this.obiettivoCalorie = totalCalories;
-          this.grassi = (this.obiettivoCalorie * 0.30) / 9;
-          this.proteine = (this.obiettivoCalorie * 0.25) / 4;
-          this.carboidrati = (this.obiettivoCalorie * 0.45) /4;
-        } else if (this.obiettivo === 'aumentare') {
-          // Aumentare il Peso (Gain Weight) option selected
-          this.obiettivoCalorie = totalCalories + 250;
           this.grassi = (this.obiettivoCalorie * 0.25) / 9;
-          this.proteine = (this.obiettivoCalorie * 0.50) / 4;
-          this.carboidrati = (this.obiettivoCalorie * 0.30) /4;
+          this.proteine = (this.obiettivoCalorie * 0.25) / 4;
+          this.carboidrati = (this.obiettivoCalorie * 0.50) /4;
+        } else if (this.obiettivo === 'aumentare') {
+            // Aumentare il Peso (Gain Weight) option selected
+            this.obiettivoCalorie = totalCalories + 250; // Calorie obiettivo per perdere peso
+        
+            // Calcolo delle proteine in grammi
+            // 2.1 grammi di proteine per chilogrammo di peso corporeo
+            this.proteine = 1.7 * this.peso; // Peso corporeo in kg
+           
+            // Calcolo delle calorie provenienti dalle proteine
+            const proteineS = this.proteine * 4; // 1g proteine = 4 calorie
+            
+            // Calcolo dei grassi in grammi
+            // 30% delle calorie totali provengono dai grassi
+            const calorieGrassi = 0.25 * this.obiettivoCalorie;
+            this.grassi = calorieGrassi / 9; // 1g grassi = 9 calorie
+          
+            // Calcolo dei carboidrati in grammi
+            // Il resto delle calorie provengono dai carboidrati
+            const calorieCarboidrati = proteineS + calorieGrassi;
+            const totaleCalarie = this.obiettivoCalorie - calorieCarboidrati 
+            this.carboidrati = totaleCalarie / 4; // 1g carboidrati = 4 calorie
+          
         } else {
           // Default or invalid selection
           this.obiettivoCalorie = totalCalories;
