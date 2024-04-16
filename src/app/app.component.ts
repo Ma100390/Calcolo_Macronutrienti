e4fdcfc9e28352db0f44ea597611f8c6065ec14b
Manuel <magini15@gmail.com>
2024-04-16 12:28:09 +0200

    Aggiornament

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,98 +28,130 @@ export class AppComponent {
   resultMessage4: string = '';
   resultMessage5: string = '';
   resultMessage6: string = '';
-
+  resultMessage7: string = '';
   obiettivoCalorie: number | undefined;
   calcoloP: number | undefined;
-
+  resetFormFields(): void {
+    this.age = undefined;
+    this.peso = undefined;
+    this.gender = undefined;
+    this.altezza = undefined;
+    this.giorni = undefined;
+    this.obiettivo = undefined;
+  
+    
+    this.resultMessage1 = '';
+    this.resultMessage2 = '';
+    this.resultMessage3 = '';
+    this.resultMessage4 = '';
+    this.resultMessage5 = '';
+    this.resultMessage6 = '';
+    this.resultMessage7 = '';
+    this.obiettivoCalorie = undefined;
+    this.calcoloP = undefined;
+    this.grassi = undefined;
+    this.proteine = undefined;
+    this.carboidrati = undefined;
+  }
+  reset(): void{
+    this.resultMessage = '';
+  }
   calculateBMI(): void {
-   if (this.age && this.peso && this.gender && this.altezza && this.giorni && this.obiettivo) {
-      let bmr: number | undefined;
+    if (this.age && this.peso && this.gender && this.altezza && this.giorni && this.obiettivo) {
+      if (this.age < 18 || this.age > 80 || this.peso < 45 || this.peso > 150 || this.altezza < 120 || this.altezza > 250) {
+        this.resultMessage = 'Dati non validi o requisiti non soddisfatti.';
+        this.resetFormFields();
+        return;
+      }
+      const pesoRegex = /^[0-9]+(\.[0-9])?$/; // Regex per consentire al massimo una cifra dopo la virgola
+      if (!pesoRegex.test(this.peso.toString())) {
+        this.resultMessage = 'Il formato del peso non è valido. Inserisci al massimo una cifra dopo la virgola.';
+        this.resetFormFields();
+        return;
+      }
 
+      let bmr: number | undefined;
+  
       if (this.gender === 'male') {
         // Calcolo BMR per gli uomini
-        bmr = (10 * this.peso) + 5 + (6.25 * this.altezza) - (5 * this.age) ;
+        bmr = (10 * this.peso) + 5 + (6.25 * this.altezza) - (5 * this.age);
       } else if (this.gender === 'female') {
         // Calcolo BMR per le donne
         bmr = (10 * this.peso) + (6.25 * this.altezza) - (5 * this.age) - 161;
       } else {
         // Genere non specificato (gestione errore)
         this.resultMessage = 'Genere non specificato.';
+        this.resetFormFields();
         return;
       }
-
+  
       if (bmr) {
         // Calcolo del fabbisogno calorico totale
         const totalCalories = bmr * this.giorni;
-        if (this.obiettivo === 'perdere') {
+        if (this.obiettivo === 'Definizione') {
           // Perdere Peso (Lose Weight) option selected
-          this.obiettivoCalorie = totalCalories - 500; // Calorie obiettivo per perdere peso
-        
+          this.obiettivoCalorie = totalCalories - 500;
+  
           // Calcolo delle proteine in grammi
-          // 2.1 grammi di proteine per chilogrammo di peso corporeo
-          this.proteine = 2.1 * this.peso; // Peso corporeo in kg
-         
+          this.proteine = 2.1 * this.peso;
+  
           // Calcolo delle calorie provenienti dalle proteine
-          const proteineS = this.proteine * 4; // 1g proteine = 4 calorie
-          
+          const proteineCalories = this.proteine * 4;
+  
           // Calcolo dei grassi in grammi
-          // 30% delle calorie totali provengono dai grassi
-          const calorieGrassi = 0.30 * this.obiettivoCalorie;
-          this.grassi = calorieGrassi / 9; // 1g grassi = 9 calorie
-        
+          const fatCalories = 0.30 * this.obiettivoCalorie;
+          this.grassi = fatCalories / 9;
+  
           // Calcolo dei carboidrati in grammi
-          // Il resto delle calorie provengono dai carboidrati
-          const calorieCarboidrati = proteineS + calorieGrassi;
-          const totaleCalarie = this.obiettivoCalorie - calorieCarboidrati 
-          this.carboidrati = totaleCalarie / 4; // 1g carboidrati = 4 calorie
-        
-        } else if (this.obiettivo === 'nulla') {
+          const carbCalories = this.obiettivoCalorie - proteineCalories - fatCalories;
+          this.carboidrati = carbCalories / 4;
+        } else if (this.obiettivo === 'Mantenimento') {
           // Mantenere Peso (Maintain Weight) option selected
           this.obiettivoCalorie = totalCalories;
           this.grassi = (this.obiettivoCalorie * 0.25) / 9;
           this.proteine = (this.obiettivoCalorie * 0.25) / 4;
-          this.carboidrati = (this.obiettivoCalorie * 0.50) /4;
-        } else if (this.obiettivo === 'aumentare') {
-            // Aumentare il Peso (Gain Weight) option selected
-            this.obiettivoCalorie = totalCalories + 250; // Calorie obiettivo per perdere peso
-        
-            // Calcolo delle proteine in grammi
-            // 2.1 grammi di proteine per chilogrammo di peso corporeo
-            this.proteine = 1.7 * this.peso; // Peso corporeo in kg
-           
-            // Calcolo delle calorie provenienti dalle proteine
-            const proteineS = this.proteine * 4; // 1g proteine = 4 calorie
-            
-            // Calcolo dei grassi in grammi
-            // 30% delle calorie totali provengono dai grassi
-            const calorieGrassi = 0.25 * this.obiettivoCalorie;
-            this.grassi = calorieGrassi / 9; // 1g grassi = 9 calorie
-          
-            // Calcolo dei carboidrati in grammi
-            // Il resto delle calorie provengono dai carboidrati
-            const calorieCarboidrati = proteineS + calorieGrassi;
-            const totaleCalarie = this.obiettivoCalorie - calorieCarboidrati 
-            this.carboidrati = totaleCalarie / 4; // 1g carboidrati = 4 calorie
-          
+          this.carboidrati = (this.obiettivoCalorie * 0.50) / 4;
+        } else if (this.obiettivo === 'Massa') {
+          // Aumentare il Peso (Gain Weight) option selected
+          this.obiettivoCalorie = totalCalories + 250;
+  
+          // Calcolo delle proteine in grammi
+          this.proteine = 1.7 * this.peso;
+  
+          // Calcolo delle calorie provenienti dalle proteine
+          const proteineCalories = this.proteine * 4;
+  
+          // Calcolo dei grassi in grammi
+          const fatCalories = 0.25 * this.obiettivoCalorie;
+          this.grassi = fatCalories / 9;
+  
+          // Calcolo dei carboidrati in grammi
+          const carbCalories = this.obiettivoCalorie - proteineCalories - fatCalories;
+          this.carboidrati = carbCalories / 4;
         } else {
-          // Default or invalid selection
+          // Opzione non valida
           this.obiettivoCalorie = totalCalories;
         }
-      
+  
         // Genera il messaggio di risultato
-        this.resultMessage1 = `Il tuo BMR è: ${bmr.toFixed(2)} kcal al giorno.`;
-        this.resultMessage2 += ` Il tuo fabbisogno calorico totale è: ${totalCalories.toFixed(2)} kcal al giorno.`;
-        this.resultMessage3 += ` Calorie finali sono: ${this.obiettivoCalorie.toFixed(2)} kcal al giorno.`;
-        this.resultMessage4 += ` Totale grammi di grassi è: ${this.grassi?.toFixed(2)} g al giorno.`;
-        this.resultMessage5 += ` Totale grammi di proteine è: ${this.proteine?.toFixed(2)} g al giorno.`;
-        this.resultMessage6 += ` Totale grammi di carboidrati è: ${this.carboidrati?.toFixed(2)} g al giorno.`;
-      } else  {
+        this.resultMessage1 = `METABOLISMO BASALE: ${bmr.toFixed(2)} KCAL.`;
+        this.resultMessage2 = `FABBISOGNO CALORICO: ${totalCalories.toFixed(2)} KCAL.`;
+        this.resultMessage3 = `Calorie per il tuo obiettivo di '${this.obiettivo}': ${this.obiettivoCalorie.toFixed(2)} KCAL.`;
+        this.resultMessage7 = 'MACRONUTRIENTI';
+        this.resultMessage6 = `Carboidrati: ${this.carboidrati?.toFixed(2)} gr.`;
+        this.resultMessage5 = `Proteine: ${this.proteine?.toFixed(2)} gr.`;
+        this.resultMessage4 = `Grassi: ${this.grassi?.toFixed(2)} gr.`;
+      } else {
         // Errore durante il calcolo del BMR (gestione errore)
-        this.resultMessage = 'Impossibile calcolare il BMR. Genere non specificato.';
+        this.resultMessage = 'Impossibile calcolare.';
+        this.resetFormFields();
       }
     } else {
       // Dati incompleti (gestione errore)
       this.resultMessage = 'Per favore inserisci tutti i dati necessari.';
+      this.resetFormFields();
     }
+   
   }
+  
 }
